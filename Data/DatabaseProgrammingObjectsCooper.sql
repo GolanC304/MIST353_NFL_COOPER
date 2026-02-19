7b67f833ebf026e5d5b83f3ac1aa561360588f44
GolanC304 <lgc00012@mix.wvu.edu>
2026-02-19 11:31:50 -0500

    sqls

--- Data/DatabaseProgrammingObjectsCooper.sql
+++ Data/DatabaseProgrammingObjectsCooper.sql
@@ -7,6 +7,7 @@ select distinct * from team;
 select count(t.TEAMID) as "ID's"
 from TEAM as t;
 
-select t.TEAMNAME
+ select t.TEAMNAME
 from TEAM as t
-where t.ConferenceDivision='AFC';
+inner join ConferenceDivision as CD
+where CD.Conference='AFC';
