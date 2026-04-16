a7b479b6aa3460a2568041d134adfc63c9330cb0
GolanC304 <lgc00012@mix.wvu.edu>
2026-04-16 12:36:58 -0400

    startup

--- startup.sh
+++ startup.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
-# Find the environment regardless of version number
-source /home/site/wwwroot/antenv/bin/activate
 
-export PYTHONPATH=$PYTHONPATH:/home/site/wwwroot
+# Move to the directory where this script lives
+cd "$(dirname "$0")"
 
-# Start the apps
+# Try to find the virtual env, if not found, install packages directly
+if [ -d "antenv" ]; then
+    source antenv/bin/activate
+else
+    python3 -m pip install -r requirements.txt
+fi
+
+export PYTHONPATH=$PYTHONPATH:$(pwd)
+
+# Start API (Port 8000)
 python3 -m gunicorn -w 2 -k uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 API.nfl_playoffs_api:app --daemon
+
+# Start UI (Port 8080)
 python3 -m streamlit run UI/nfl_playoffs_ui.py --server.port 8080 --server.address 0.0.0.0
