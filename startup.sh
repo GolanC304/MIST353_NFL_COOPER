2a59141715a5b3625e304a4e5d711ee1eeb26532
GolanC304 <lgc00012@mix.wvu.edu>
2026-04-16 12:29:24 -0400

    newnew

--- startup.sh
+++ startup.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
+# Force Azure to use its own virtual environment
+source /home/site/wwwroot/antenv/bin/activate
 
-# Use the absolute path for the site root
 export PYTHONPATH=$PYTHONPATH:/home/site/wwwroot
 
-# Start the FastAPI backend on port 8000
-# -D runs gunicorn in 'daemon' mode (true background)
-gunicorn -w 2 -k uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 API.nfl_playoffs_api:app --daemon
+# Start API
+gunicorn -w 2 -k uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 API.nfl_playoffs_api:app &
 
-# Start the Streamlit frontend
-# If $PORT is empty, it will default to 8080
-streamlit run UI/nfl_playoffs_ui.py --server.port ${PORT:-8080} --server.address 0.0.0.0
+# Start UI
+streamlit run UI/nfl_playoffs_ui.py --server.port 8080 --server.address 0.0.0.0
