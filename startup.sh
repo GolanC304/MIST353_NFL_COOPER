d5dae8681b8b653284b3c3dce5af0e0cc87029d7
GolanC304 <lgc00012@mix.wvu.edu>
2026-04-16 12:34:03 -0400

    newfile

--- startup.sh
+++ startup.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-# Force Azure to use its own virtual environment
+# Find the environment regardless of version number
 source /home/site/wwwroot/antenv/bin/activate
 
 export PYTHONPATH=$PYTHONPATH:/home/site/wwwroot
 
-# Start FastAPI
+# Start the apps
 python3 -m gunicorn -w 2 -k uvicorn.workers.UvicornWorker --bind=0.0.0.0:8000 API.nfl_playoffs_api:app --daemon
-
-# Start Streamlit
 python3 -m streamlit run UI/nfl_playoffs_ui.py --server.port 8080 --server.address 0.0.0.0
